2a4edfb6fbab6bfb63e6a765065c2e9fb7249184
Edgar Saavedra <edgarsaavedraa@gmail.com>
2017-08-29 10:49:42 -0700

    Binary searching example

---
+++ js/sorting/Searching/Binary.js
@@ -0,0 +1,9 @@
+/**
+ * =======================================
+ * Binary Search
+ * =======================================
+ *
+ * - Array must be sorted
+ * - This falls under divide and conquer
+ * - Find mid point and then discard any values small/larger than the item you are searching.
+ */
